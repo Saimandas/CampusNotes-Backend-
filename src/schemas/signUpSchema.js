57839e419d593adcfded9779ac2928a85fd3b0ea
Saimandas <dhirajdas55555@gmail.com>
2024-05-11 13:03:59 +0530

    first commit

--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -1,7 +1,7 @@
 import {z} from "zod"
 
 const usernameValidation=
-z.string().min(2,"username must have at least two charecter").max(20,"No more than 20 charecter")
+z.string().min(2,"username must have at least two charecter").max(12,"No more than 12 charecter")
 .regex(/(?!.*[\.\-\_]{2,})^[a-zA-Z0-9\.\-\_]{3,24}$/gm,"username can't conatain special charecter or space")
 
 const signUpSchema= z.object({
